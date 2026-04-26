56937ef17e71429cad0dea8bc3baae14ab2c5dc1
Zechariah Wang <zechariahwang@gmail.com>
2026-04-26 17:43:06 -0600

    working game loop

--- src/ServerScriptService/Hello2.server.lua
+++
@@ -1,8 +0,0 @@
-local players = game:GetService("Players")
-
-local function onPlayerAdded(player)
-    local part = game.Workspace.help
-    print("Hello, " .. player.Name .. "!")
-end
-
-players.PlayerAdded:Connect(onPlayerAdded)
